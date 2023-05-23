6e0a3a5b97f6ed6dd69c8bdce587bd2fe5d81c55
Miguel Gamboa <miguelgamboa@outlook.com>
2023-05-23 16:20:44 +0100

    Remove useless resolvers and handlers.

--- src/main/kotlin/com/jeroenreijn/examples/router/PresentationsRoutes.kt
+++ src/main/kotlin/com/jeroenreijn/examples/router/PresentationsRoutes.kt
@@ -1,23 +1,25 @@
 package com.jeroenreijn.examples.router
 
-import com.jeroenreijn.examples.handler.PresentationsRouterHandler
+import com.jeroenreijn.examples.repository.PresentationRepo
+import com.jeroenreijn.examples.view.appendable.AppendableSink
+import com.jeroenreijn.examples.view.htmlFlowTemplate
+import com.jeroenreijn.examples.view.kotlinXReactive
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.context.annotation.Bean
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.RouterFunctions
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.coRouter
+import org.springframework.web.reactive.function.server.*
+import reactor.core.publisher.Mono
 
 @Component
-class PresentationsRoutes(private val presentationsHandler: PresentationsRouterHandler) {
+class PresentationsRoutes(private val repo : PresentationRepo) {
 
     @Bean
     fun presentationsCoRouter() = coRouter {
         "/router".nest {
-            GET("/{template}/coroutine") {
-                presentationsHandler.handleTemplate(it).awaitSingle()
-            }
+            GET("/htmlFlow/coroutine") { handleTemplateHtmlFlow().awaitSingle() }
+            GET("/kotlinx/coroutine") { handleTemplateKotlinX().awaitSingle() }
         }
     }
 
@@ -26,7 +28,34 @@ class PresentationsRoutes(private val presentationsHandler: PresentationsRouterH
         .route()
         .path("/router") { builder ->
             builder
-                .GET("/{template}", presentationsHandler::handleTemplate)
+                .GET("/htmlFlow") { this.handleTemplateHtmlFlow() }
+                .GET("/kotlinx") { this.handleTemplateKotlinX() }
         }
         .build()
+
+    private fun handleTemplateHtmlFlow() : Mono<ServerResponse> {
+        val view = AppendableSink {
+                htmlFlowTemplate
+                    .writeAsync(this, repo.findAllReactive())
+                    .thenAccept {this.close()}
+            }
+            .asFlux()
+
+        return ServerResponse
+            .ok()
+            .contentType(MediaType.TEXT_HTML)
+            .body(view, object : ParameterizedTypeReference<String>() {})
+    }
+
+    private fun handleTemplateKotlinX() : Mono<ServerResponse> {
+        val view = AppendableSink {
+                kotlinXReactive(this, repo.findAllReactive())
+            }
+            .asFlux()
+        return ServerResponse
+            .ok()
+            .contentType(MediaType.TEXT_HTML)
+            .body(view, object : ParameterizedTypeReference<String>() {})
+    }
+
 }
