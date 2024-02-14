5cb2d6bb266fa215546695a5111ad5022d9f0c9a
Miguel Gamboa <miguelgamboa@outlook.com>
2024-02-14 16:44:27 +0000

    Configure repository elements timeout from the Java property e.g. for 10 millis -DbenchTimeout=10

--- src/main/java/com/jeroenreijn/examples/Launch.java
+++ src/main/java/com/jeroenreijn/examples/Launch.java
@@ -5,10 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import static java.lang.System.out;
+
+/**
+ * Set the timeout interleaving datasource items in Millis, e.g. 1 Milli:
+ *   mvn spring-boot:run -Dspring-boot.run.jvmArguments="-DbenchTimeout=10"
+ */
 @SpringBootApplication
 public class Launch {
 	public static void main(String[] args) {
-		InMemoryPresentations.Companion.setTimeout(300);
+		final var strTimeout = System.getProperty("benchTimeout");
+		final int timeout = strTimeout != null ? Integer.parseInt(strTimeout) : 300;
+		InMemoryPresentations.Companion.setTimeout(timeout);
 		SpringApplication.run(Launch.class, args);
 	}
 }
