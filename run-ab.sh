05f3e7c13d659942086f76ca99aa67bca8356da2
Miguel Gamboa <miguelgamboa@outlook.com>
2024-02-19 13:40:55 +0000

    Increase ab timeout to 240 seconds.

--- run-ab.sh
+++ run-ab.sh
@@ -8,7 +8,7 @@ for ths in "${THREADS[@]}"; do   # For each number of threads
   for path in "$@"; do           # For each path in line argument
     for ((n=0;n<$ITERATIONS;n++)); do
       total_requests=$((REQUESTS * ths))
-      result=$(ab -q -n $total_requests -c $ths http://localhost:8080/$path | grep "Time taken for tests")
+      result=$(ab -q -s 240 -n $total_requests -c $ths http://localhost:8080/$path | grep "Time taken for tests")
       echo "$path:$ths:$total_requests:$result"
     done
   done
