297aaa3e024e0cacdb9a8d19b5f8d3710b661e11
Miguel Gamboa <miguelgamboa@outlook.com>
2024-07-05 18:54:53 +0100

    Simplify Sinks and remove auxiliary BufferedSink and use Kotlin many replay sink.

--- src/main/kotlin/com/jeroenreijn/examples/view/appendable/OutputStreamSink.kt
+++ src/main/kotlin/com/jeroenreijn/examples/view/appendable/OutputStreamSink.kt
@@ -1,28 +1,27 @@
 package com.jeroenreijn.examples.view.appendable
 
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Sinks
 import java.io.Closeable
 import java.io.OutputStream
 import java.nio.charset.StandardCharsets
 
 
 class OutputStreamSink : OutputStream(), Closeable {
-    private val sink = BufferedSink()
+    private val sink = Sinks.many().replay().all<String>()
 
     fun asFlux(): Flux<String> = sink.asFlux()
 
     override fun write(b: Int) {
-        sink.bufferedWriter.write(b)
-        sink.tryFlush()
+        throw UnsupportedOperationException()
     }
 
     override fun write(b: ByteArray, off: Int, len: Int) {
-        sink.bufferedWriter.write(String(b, off, len, StandardCharsets.UTF_8))
-        sink.tryFlush()
+        sink.emitNext(String(b, off, len, StandardCharsets.UTF_8), Sinks.EmitFailureHandler.FAIL_FAST)
     }
 
     override fun close() {
         super.close()
-        sink.close()
+        sink.emitComplete(Sinks.EmitFailureHandler.FAIL_FAST)
     }
 }
--- src/main/kotlin/com/jeroenreijn/examples/view/appendable/WriterSink.kt
+++ src/main/kotlin/com/jeroenreijn/examples/view/appendable/WriterSink.kt
@@ -1,22 +1,26 @@
 package com.jeroenreijn.examples.view.appendable
 
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Sinks
 import java.io.Closeable
 import java.io.Writer
 
 class WriterSink : Writer(), Closeable {
-    private val sink = BufferedSink()
+    private val sink = Sinks.many().replay().all<String>()
 
     fun asFlux(): Flux<String> {
         return sink.asFlux()
     }
 
-    override fun close() = sink.close()
+    override fun close() {
+        sink.emitComplete(Sinks.EmitFailureHandler.FAIL_FAST)
+    }
 
-    override fun flush() = sink.tryFlush()
+    override fun flush() {
+        
+    }
 
     override fun write(cbuf: CharArray, off: Int, len: Int) {
-        sink.bufferedWriter.write(cbuf, off, len)
-        sink.tryFlush()
+        sink.emitNext(String(cbuf, off, len), Sinks.EmitFailureHandler.FAIL_FAST)
     }
 }
